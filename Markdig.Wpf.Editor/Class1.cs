8072a5d39f82fb0d4ae275fd9d6a2f783278c7cf
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-04-09 20:13:37 +0200

    Add project files.

---
+++ Markdig.Wpf.Editor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdig.Wpf.Editor
+{
+    public class Class1
+    {
+    }
+}
