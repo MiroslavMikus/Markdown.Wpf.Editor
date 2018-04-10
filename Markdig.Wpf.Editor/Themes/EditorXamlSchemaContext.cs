a8d39d263bf1cbfdcbc532ffe5ee03540ed02f7c
Miroslav Mikus <miroslav.mikus@outlook.com>
2018-04-10 12:29:15 +0200

    Add schema context

---
+++ Markdig.Wpf.Editor/Themes/EditorXamlSchemaContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Xaml;
+
+namespace Markdig.Wpf.Editor.Themes
+{
+    internal class EditorXamlSchemaContext : XamlSchemaContext
+    {
+        public override bool TryGetCompatibleXamlNamespace(string a_xamlNamespace, out string a_compatibleNamespace)
+        {
+            if (!a_xamlNamespace.Equals("clr-namespace:Markdig.Wpf"))
+                return base.TryGetCompatibleXamlNamespace(a_xamlNamespace, out a_compatibleNamespace);
+
+            a_compatibleNamespace = $"clr-namespace:Markdig.Wpf;assembly={Assembly.GetAssembly(typeof(Markdig.Wpf.Styles)).FullName}";
+
+            return true;
+        }
+    }
+}
