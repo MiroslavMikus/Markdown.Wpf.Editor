5e723b16350e509d22401eafa219b6e34b4c75c1
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-05-15 05:28:40 +0200

    update assembly info

--- Markdig.Wpf.Editor/Properties/AssemblyInfo.cs
+++ Markdig.Wpf.Editor/Properties/AssemblyInfo.cs
@@ -51,5 +51,10 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+#if DEBUG
+[assembly: AssemblyVersion("1.0.0.*")]
+#else
+[assembly: AssemblyVersion("1.0.0")]
+#endif
+
 [assembly: AssemblyFileVersion("1.0.0.0")]
